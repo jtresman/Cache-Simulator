fc47fe03a65299965926ca60c4098a2821484e96
Healy <owner2@res1-128-252-dhcp.int.colorado.edu>
2015-04-18 16:34:20 -0600

    Cool

--- cache.h
+++ cache.h
@@ -9,30 +9,23 @@
  ******************************************************
  */
 
-
-unsigned long long int l1_I_kickouts;
-unsigned long long int l1_I_kickouts_dirty;
-unsigned long long int l1_I_transfers;
-unsigned long long int l1_I_kickouts_flush;
-unsigned long long int l1_D_kickouts;
-unsigned long long int l1_D_kickouts_dirty;
-unsigned long long int l1_D_transfers;
-unsigned long long int l1_D_kickouts_flush;
-unsigned long long int l1_D_total_req;
-unsigned long long int l1_I_total_req;
+ //Simulation Globals
+unsigned long long int l1_ikickouts;
+unsigned long long int l1_ikickouts_dirty;
+unsigned long long int l1_itransfers;
+unsigned long long int l1_ikickouts_flush;
+unsigned long long int l1_dkickouts;
+unsigned long long int l1_dkickouts_dirty;
+unsigned long long int l1_dtransfers;
+unsigned long long int l1_dkickouts_flush;
+unsigned long long int l1_dtotal_req;
+unsigned long long int l1_itotal_req;
 unsigned long long int l2_total_req;
 unsigned long long int l2_kickouts;
 unsigned long long int l2_kickouts_dirty;
 unsigned long long int l2_transfers;
 unsigned long long int l2_kickouts_flush;
-unsigned long int l1_I_Misses;
-unsigned long long int l1_I_Hits;
-unsigned long int l1_D_Misses;
-unsigned long long int l1_D_Hits;
-unsigned long long int l2_Hits;
-unsigned long long int l2_Misses;
 
-//Percentage parameters
 float read_ref_perc;
 float write_ref_perc;
 float inst_ref_perc;
@@ -41,19 +34,18 @@ float write_cycle_perc;
 float inst_cycle_perc;
 float avg_cyc_write;
 float avg_cyc_inst;
+float avg_cyc_read;
 
 unsigned long long int avg_cyc_exec_time;
 float avg_cyc_cpi;
 unsigned long long int avg_cyc_exec_misalign;
 float avg_cyc_cpi_misalign;
-
-//Cost parameters
-unsigned long long int l1_I_cost;
-unsigned long long int l1_D_cost;
+unsigned long long int l1_icost;
+unsigned long long int l1_dcost;
 unsigned long long int l2_cost;
 unsigned long long int mem_cost;
 unsigned long long int tot_cost;
-unsigned long long int flushes;
+int flushes;
 unsigned long long int invalidates;
 
 // Default Cache Parameters
@@ -63,9 +55,10 @@ unsigned long long int invalidates;
 //Default L1 Cache Parameters
 #define L1_DEFAULT_CACHE_SIZE 8192
 #define L1_DEFAULT_CACHE_BLOCK_SIZE 32
-#define L1_DEFAULT_CACHE_ASSOC 2
+#define L1_DEFAULT_CACHE_ASSOC 1
 #define L1_HIT_TIME 1
 #define L1_MISS_TIME 1
+#define L1_OFFSET 5
 
 //Default L2 Cache Parameters
 #define L2_DEFAULT_CACHE_SIZE 32768
@@ -75,6 +68,9 @@ unsigned long long int invalidates;
 #define L2_MISS_TIME 7
 #define L2_TRANSFER_TIME 5
 #define L2_BUS_WIDTH 16
+#define MEM_TO_L2 160
+#define L2_TO_L1 10
+#define L2_OFFSET 6
 
 //Default Main Memory Access Times
 #define MEM_SENDADDR 10
@@ -84,50 +80,39 @@ unsigned long long int invalidates;
 
 //Setup Default Cache Parameters
 int l1_cache_size;
-int l2_cache_size;
+int l2_cache_size; 
 int l1_cache_block_size;
-int l2_cache_block_size; 
-// int l1_words_per_block = L1_DEFAULT_CACHE_BLOCK_SIZE / WORD_SIZE;
-// int l2_words_per_block = L2_DEFAULT_CACHE_BLOCK_SIZE / WORD_SIZE;
-
-//Cache association
+int l2_cache_block_size;
 int l1_cache_assoc;
 int l2_cache_assoc;
 
  //Simulation Globals
-
-unsigned long long int l1_hit_time = L1_HIT_TIME;
-unsigned long long int l1_miss_time = L1_MISS_TIME;
-unsigned long long int l2_hit_time = L2_MISS_TIME;
-unsigned long long int l2_miss_time = L2_MISS_TIME;
-unsigned long long int l2_transfer_time = L2_TRANSFER_TIME;
-unsigned long long int l2_bus_width = L2_BUS_WIDTH;
 unsigned long long int execution_time;
 unsigned long long int flush_time;
 unsigned long long int inst_refs;
-unsigned long long int data_refs;
 unsigned long long int read_refs;
 unsigned long long int write_refs;
 unsigned long long int read_cycle;
 unsigned long long int write_cycle;
 unsigned long long int inst_cycle;
-unsigned long long int l1_I_misses;
-unsigned long long int l1_I_hits;
-unsigned long long int l1_D_misses;
-unsigned long long int l1_D_hits;
+unsigned long long int l1_imisses;
+unsigned long long int l1_ihits;
+unsigned long long int l1_dmisses;
+unsigned long long int l1_dhits;
 unsigned long long int l2_hits;
 unsigned long long int l2_misses;
 unsigned long long int total_inst;
-
-float l1_I_hit_rate;
-float l1_I_miss_rate;
-float l1_D_hit_rate;
-float l1_D_miss_rate;
+float l1_ihit_rate;
+float l1_imiss_rate;
+float l1_dhit_rate;
+float l1_dmiss_rate;
 float l2_hit_rate;
 float l2_miss_rate;
 
 int l1_cache_lines;
 int l2_cache_lines;
+int shift_amount_l1;
+int shift_amount_l2;
 
 //Cache Entry Structure
 typedef struct cache_entry {
@@ -139,8 +124,8 @@ typedef struct cache_entry {
     struct cache_entry *prev;
 } cache_entry;
 
-struct cache_entry **l1_I_cache;
-struct cache_entry **l1_D_cache;
+struct cache_entry **l1_icache;
+struct cache_entry **l1_dcache;
 struct cache_entry **l2_cache;
 
 // Function Prototypes
